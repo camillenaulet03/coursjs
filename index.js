dca2874b5d72297b0ab70c6f7a998911a75cee72
ArthurRolland1256 <45195704+ArthurRolland1256@users.noreply.github.com>
2019-01-24 11:56:44 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-function popup (identifiant) {
-    var temp = identifiant;
-    //alert('1');
-    if (document.getElementById(temp).style.display === "block") {//Donc le bloc est affiché, on le masque
-        document.getElementById(temp).style.display = "none";
-        //alert('2');
-        document.getElementById('fade').style.display = "none";
+function popup(identifiant) {
+    var temp=identifiant;
+        //alert ('1');
+    if (document.getElementById(temp).style.display==="block") {//Donc le bloc est affiché, on le masque
+        document.getElementById(temp).style.display="none";
+       // alert('2');
+        document.getElementById('fade').style.display="none";
         //alert('3');
     } else {//Donc le bloc est masqué, on l'affiche
-        document.getElementById(temp).style.display = "block";
+        document.getElementById(temp).style.display="block";
         //alert('4');
-        document.getElementById('fade').style.display = "block";
+        document.getElementById('fade').style.display="block";
         //alert('5');
     }
 }
-
-$("#bouton").bind("click", popup('light'));
-$("#fermer").bind("click", popup('light'));
-$("#annuler").bind("click", popup('light'));
+document.querySelector('#bouton').addEventListener('click', (event) => {
+    popup('light')
+})
+document.querySelector('#annuler').addEventListener('click', (event) => {
+    popup('light')
+})
 
 function validateEmail(email) {
-    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var re = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
     return re.test(email);
 }
-
 function validate() {
     var $result = $("#result");
     var email = $("#email").val();
-
     if (validateEmail(email)) {
         $result.text(email + " est valide :)");
         $result.css("color", "green");
@@ -36,5 +36,4 @@ function validate() {
     }
     return false;
 }
-
 $("#validate").bind("click", validate);
