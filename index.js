9fb7828251eb0938246630930805c7e58b616ed5
camillenaulet03 <45195515+camillenaulet03@users.noreply.github.com>
2019-01-25 10:27:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 function popup (identifiant) {
     var temp = identifiant;
-    //alert('1');
     if (document.getElementById(temp).style.display === "block") {//Donc le bloc est affiché, on le masque
         document.getElementById(temp).style.display = "none";
-        //alert('2');
         document.getElementById('fade').style.display = "none";
-        //alert('3');
     } else {//Donc le bloc est masqué, on l'affiche
         document.getElementById(temp).style.display = "block";
-        //alert('4');
         document.getElementById('fade').style.display = "block";
-        //alert('5');
     }
 }
 
